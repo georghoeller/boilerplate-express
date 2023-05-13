24f866d82af26d78bb5a7bf614d171cc07b1432a
wlnhoge <georg.hoeller@raiffeisenbank.at>
2023-05-13 18:35:17 +0200

    update

--- myApp.js
+++ myApp.js
@@ -1,35 +1,40 @@
+require('dotenv').config();
+
+
 let express = require('express');
 let app = express();
 
-require('dotenv').config()
 
 console.log('Hello World');
+
+app.use( function middleware(req,res,next){
+    const middlewareLogger = req.method + " " + req.path + " - " +req.ip;
+    console.log(middlewareLogger);
+    next();
+});
 
 
 // serve the index.html file as a main page 
 app.get('/', function(req, res) {
-    console.log(__dirname);
+    console.log('dirname',__dirname);
     const absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
   });
 
-// app.use is for ressources to be used > here css styles are imported
-app.use('/public', express.static(__dirname+'/public') );
-
+  
 app.get('/json', function(req,res){
     
-    let message;
     console.log(process.env.MESSAGE_STYLE);
-
+    
     if (process.env.MESSAGE_STYLE === 'uppercase') {
-        message = 'Hello Json'.toUpperCase();
-    } else {
-        message = 'Hello Json';
+       res.json({"message": 'Hello json'.toUpperCase() });
+    } else {  
+      res.json({"message": 'Hello json' });
     }
+    
+  });
 
-    const messageOject = {"message": message };
-    res.json(messageOject);
-});
-
+// app.use is for ressources to be used > here css styles are imported
+app.use('/public', express.static(__dirname+'/public') );
 
  module.exports = app;
